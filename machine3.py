d9dc589bf93979b681d770782d66aa6be43fdf0d
Aedan Cullen <aedancullen@gmail.com>
2017-09-16 20:23:07 -0400

    Add definition of Stream count

--- machine3.py
+++ machine3.py
@@ -8,14 +8,14 @@ class PCMAudioStreamer(a.LocalComponent):
 
     def run(self, input, progress_callback=None):
         wf = wave.open("audio.wav", 'rb')
-        data = wf.readframes(1024)
+        data = wf.readframes(input.count)
         while data != b'':
             data_spliced = [data[x:x+4] for x in range(0, len(data),4)]
             stream = a.Stream.from_bytes(data_spliced)
             print("Sending a.Stream containing:")
             print(stream.items)
             progress_callback(stream)
-            data = wf.readframes(1024)
+            data = wf.readframes(input.count)
         return a.Stream.blankFromCountParameters(input.count, 4)
 
 
